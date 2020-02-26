9d8204404ecc89a5c167a6a1574a52ff37f6b9a5
bamba-sudo <bambasudo@yahoo.com>
2020-02-26 07:48:47 -0500

    script bash

---
+++ os.py
@@ -0,0 +1,10 @@
+###################################
+# Auteur : Walid Gharbi			  #
+# But : Demarer un fichier bash...#
+###################################
+
+
+import os
+
+os.system("echo walid")
+		
